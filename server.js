dc73f406ac31c4f2518f3a7f19edd09e0e1cb028
Chester Springwood <jackieAmartinez@gmail.com>
2023-05-30 18:07:41 -0400

    declarations and imports updated

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-// example code
+// example code and further declarations/imports
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
+const fs = require('fs');
+const db = require('./db/db.json');
+const route = require("./routes/routes")
 
 // initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set up data formats (body parsing, static, and route middleware) so that express can parse
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use('/api', apiRoutes);
+app.use('/', route);
 
-// set up body parsing, static, and route middleware
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json());
-
-app.use('/api', apiRoutes);
-
-app.use('/', htmlRoutes);
-
-// start the server on the port
+// notifies express to listen for activity, so that the server on the port can start
 app.listen(PORT, () => {
-    console.log('Server available at local${PORT}');
+    console.log('You did it! Server available at local${PORT}');
 });
