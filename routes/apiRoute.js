0a1dfcecc1b07464dfd8c9e305a81ebefd159372
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-06 19:49:02 -0400

    routes finally fixedgit add .

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,43 +1,32 @@
-const path = require('path');
-const fs = require('fs');
-const router = require('express').Router();
-let db = require('../db/db.json')
-//const uuid = require('uuid');
+const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
-// .no dot pre notes
-router.get('/notes', (req, res) => {
-        res.sendFile(path.join(dirname, '../public/notes.html'));
-    });
-// . dots gotta go
-router.get('/', (req, res) => {
-        res.sendFile(path.join(dirname, '../public/index.html'));
-    });
+let db = require("../db/db.json");
+const fs = require("fs");
 
-router.get('/api/notes', (req, res) => {
-    db = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
-        res.json(db)
-    });
+router.get("/notes", (req, res) => {
+  db = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  res.json(db);
+});
 
-router.post('/api/notes', (req, res) => {
-    const {title, text} = req.body; 
-    const newNote = {
-        title: title,
-        text: text,
-        id: uuidv4()
-    };
+router.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+  const newNote = {
+    title: title,
+    text: text,
+    id: uuidv4(),
+  };
 
-    db.push(newNote);
-    fs.writeFileSync(path.join(__dirname, "../db/db.json"),
-    JSON.stringify(db));
+  db.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(db));
 
-    res.json(db);
- });
+  res.json(db);
+});
 
-router.delete('/api/notes/:id', (req, res) => {
-    let db = JSON.parse(fs.readFileSync('./db/db.json'))
-    let trashNote = db.filter(item => item.id != req.params.id);
-    fs.writeFileSync('./db/db.json', JSON.stringify(trashNote));
-    res.json(trashNote);
+router.delete("/notes/:id", (req, res) => {
+  let db = JSON.parse(fs.readFileSync("db/db.json"));
+  let trashNote = db.filter((item) => item.id !== req.params.id);
+  fs.writeFileSync("db/db.json", JSON.stringify(trashNote));
+  res.json(trashNote);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,17 @@
-// example code and further declarations/imports
-const express = require('express');
-const app = express();
-// const path = require('path');
-const fs = require('fs');
-const PORT = process.env.PORT || 3001;
-// const db = require('./db/db.json');
-
-const db = require('./db/db.json')
-
+const express = require("express");
 const apiRoutes = require("./routes/apiRoute");
+const htmlRoutes = require("./routes/htmlRoute");
 
-// initialize the app and create a port
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3002;
 
-
-const { v4: uuidv4 } = require("uuid");
-// set up data formats (body parsing, static, and route middleware) so that express can parse
+// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.use(express.static('public'));
-// app.use('/api', apiRoutes);NOT NEEDED
-// app.use('/api', apiRoutes);NOT NEEDED
-app.use('/', apiRoutes);
-
-// notifies express to listen for activity, so that the server on the port can start
-app.listen(PORT, () => {
-    console.log('You did it! Server available at local${PORT}');
-});
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
